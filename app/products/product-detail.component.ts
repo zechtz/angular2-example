aaea81abf8a7b76f69ac4c267061c69cb35d3d51
Zachariah Ngonyani <zachariahn@gmail.com>
2016-06-28 01:21:53 +0300

    Add routing and create product details template and component

---
+++ app/products/product-detail.component.ts
@@ -0,0 +1,9 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  templateUrl: './product-detail.component.html'
+})
+
+export class ProductDetail{
+  pageTitle: string = 'Product Detail';
+}
