701cc1251378f3a1b0e1b65e1a8cc1a476247a54
Zachariah Ngonyani <zachariahn@gmail.com>
2016-06-27 15:41:05 +0300

    set up app component and boot component

---
+++ app/boot.ts
@@ -0,0 +1,5 @@
+import {bootstrap}    from 'angular2/platform/browser'
+import {AppComponent} from './app.component'
+import {ProductListComponent} from './products/product-list.component'
+
+bootstrap(AppComponent);
