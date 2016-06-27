074989bc712eef3e77edd9dd5d98f6779b933374
Zachariah Ngonyani <zachariahn@gmail.com>
2016-06-28 01:22:31 +0300

    inject routes and route configuration to app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component }            from 'angular2/core';
-import { HTTP_PROVIDERS }       from 'angular2/http';
-import { 'rxjs/Rx' };           //load all features 
+import { Component }                     from 'angular2/core';
+import { HTTP_PROVIDERS }                from 'angular2/http';
+import { 'rxjs/Rx' };
+import { ROUTER_PROVIDERS, RouteConfig } from 'angular2/router';
 
-import { ProductListComponent } from './products/product-list.component';
-import { ProductService }       from './products/product.service';
+import { ProductListComponent }          from './products/product-list.component';
+import { ProductService }                from './products/product.service';
+import { WelcomeComponent }              from './home/welcome.component';
 
 @Component({
   selector: 'pm-app',
@@ -14,8 +16,13 @@ import { ProductService }       from './products/product.service';
       </div>
     `,
     directives:   [ProductListComponent],
-   providers:     [ProductService, HTTP_PROVIDERS]
+   providers:     [ProductService, HTTP_PROVIDERS, ROUTER_PROVIDERS]
 })
+
+@RouteConfig([
+  { path: '/welcome', name: 'Welcome', component: WelcomeComponent, useAsDefault: true }
+  { path: '/products', name: 'Products', component: ProductListComponent }
+])
 export class AppComponent{
   pageTitle: string = "Product Management";
 }
